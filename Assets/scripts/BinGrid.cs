6eb2856c9a295fcd30cd419c89f7e699a2838004
Victoria Veselichka <vickyveselichka@gmail.com>
2020-09-21 15:09:39 +0100

    added comments to scripts

--- Assets/scripts/BinGrid.cs
+++ Assets/scripts/BinGrid.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 public class BinGrid<T> {
 
+	// class representing a cube array of bins which segment space into many cubes
+	// centered on the origin
+
 	public LinkedList<T>[] bins;
 	public int nBins;
 	public int[] offsets;
@@ -16,20 +19,25 @@ public class BinGrid<T> {
 		sideLength = inSideLength;
 		binLength  = inBinLength;
 
+		// calculate the number of bins needed to cover a cube of sideLength
 		binsPerEdge = (int)( sideLength/binLength + 1 );
 		nBins = binsPerEdge*binsPerEdge*binsPerEdge;
 
+		// populate the bins array with linked lists of T
 		bins = new LinkedList<T>[nBins];
 
 		for(int i=0; i<nBins; ++i) {
 			bins[i] = new LinkedList<T>();
 		}
 
+		// these integer offsets are what needs to be added to an index of a bin to get to the 6 bordering bins
 		offsets = new int[] {0, 1, -1, binsPerEdge, -binsPerEdge, binsPerEdge*binsPerEdge, -binsPerEdge*binsPerEdge};
 	}
 
+	// allows the BinGrid object to be indexed directly returning the bin at that index
 	public LinkedList<T> this[int i] => bins[i];
 
+	// calculate the index of the bin that pos is within
 	public int indexAtPosition(Vector3 pos) {
 		
 		Vector3 fromBottomCorner = pos + Vector3.one * sideLength * 0.5f;
--- Assets/scripts/Boid.cs
+++ Assets/scripts/Boid.cs
@@ -10,6 +10,7 @@ public class Boid : MonoBehaviour {
 	private LinkedListNode<Boid> listNode;
 	private int binIndex;
 
+	// functions to make position constraint within the box easier
 	private float mod(float a, float b) => a - b * Mathf.Floor(a / b);
 	private float loop(float t, float min, float max) => mod(t - min, max - min) + min;
 	private float loop30(float t) => loop(t, -15.0f, 15.0f);
@@ -27,54 +28,75 @@ public class Boid : MonoBehaviour {
 
 	public void Setup(BoidCoordinator inBoidCoordinator, BinGrid<Boid> inBoidBins, int inBinIndex, LinkedListNode<Boid> inListNode) {
 		
+		// just sets certain variables in the boid 
 		boidCoordinator = inBoidCoordinator;
 		boidBins = inBoidBins;
 		listNode = inListNode;
 		binIndex = inBinIndex;
 
+		// initial random velocity
 		vel = Random.onUnitSphere * 10.0f;
 	}
 
 	public void FixedUpdate() {
 
-		uint c = 0;
+		// update the boid's velocity based on the boids around it
+
+		uint c = 0; // counts how many surrounding boids we've looked at
+
+		// loop over the nearby bins in the BinGrid
 		foreach(int offset in boidBins.offsets) {
 
 			int i = binIndex + offset;
+
+			// ignore indices that are outside the BinGrid
 			if(i < 0 || i >= boidBins.nBins) continue;
 
+			// loop over the boids in those bins
 			foreach(Boid boid in boidBins[i]) {
 
+				// get displacement vector of the other boid
 				Vector3 dis = boid.pos - pos;
+
+				// increment velocity based on boid spacing and velocity alignment
 				vel += dis.normalized * attract( dis.magnitude ) * 0.003f;
 				vel += boid.vel * 0.003f;
 
+				// if we've considered more than 10 surrounding boids already then end the loop
 				if(++c > 10) break;
 			}
 
+			// break out of lower loop if we've looked at 10 surrounding boids
 			if(c > 10) break;
 		}
 
+		// create a repulsion force from the walls approaching infinity if the boid is at the wall
 		foreach(Plane wall in boidCoordinator.walls) {
 
 			float dist = wall.GetDistanceToPoint(pos);
 			vel += 0.2f/dist * wall.normal;
 		}
 
+		// fix the boid's speed to be 5m/s and update the position
 		vel = vel.normalized * 5f;
 
 		pos += vel * Time.deltaTime;
 
 
+		// constrain the position to be within a 30x30x30 cube
 		pos = new Vector3(loop30(pos.x),
 						  loop30(pos.y),
 						  loop30(pos.z));
 		
+		// set te rotation to be aligned with the velocity vector
 		rot = Quaternion.LookRotation( vel );
 
 
+		// update which bin the boid is in
 		int newBinIndex = boidBins.indexAtPosition( pos );
 
+		// if the boid has crossed into a new bin then remove it's listNode from the original bin
+		// and make a new one for it at the end of the new bin's linked list
 		if(newBinIndex != binIndex) {
 
 			boidBins[binIndex].Remove( listNode );
